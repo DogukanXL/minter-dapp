d9f6aefc4a046c9f486760b7577f532228b3a3c1
DogukanXL <doguceremey@gmail.com>
2022-07-21 01:24:59 +0300

    update mint

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-07-21T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-07-23T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x36819877c03bB3Cd1ac767e9e6b2aEfDfe55C752"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
