cb947a189658d1cc9d6a8fd206568da236ef8437
muzdeneme <muzdeneme123@gmail.com>
2022-07-26 00:34:52 +0300

    update config

--- backend/src/config.js
+++ backend/src/config.js
@@ -75,7 +75,7 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
 const GENERIC_DESCRIPTION = "DAYI NAPTIN? "; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreibkcddhm3h5f7eopswo35g3zxg3v6p57okfv5kflar3hczhg54ydu"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreifg3iwc6mdxomv6zmolhm2q2jc566vmqpptwsuyngwhbob7rifeoy"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
